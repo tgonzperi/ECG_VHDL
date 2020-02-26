95aa2230e51e524a9d455a0c351210bbc0f21ceb
tomas <tgonzperi@gmail.com>
2020-02-26 21:11:26 +0100

    added output and substraction to accumulator

--- projet_ecg.srcs/sources_1/new/FIR_OP.vhd
+++ projet_ecg.srcs/sources_1/new/FIR_OP.vhd
@@ -1,21 +1,21 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
+-- Company:
+-- Engineer:
+--
 -- Create Date: 02/26/2020 11:35:34 AM
--- Design Name: 
+-- Design Name:
 -- Module Name: FIR_OP - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--
 ----------------------------------------------------------------------------------
 
 
@@ -31,43 +31,53 @@ use IEEE.NUMERIC_STD.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity FIR_OP is
+entity accumulator is
     generic(
         N       : integer := 24;
         n_out   : integer := 24
     );
     port(
-        clk, rst                                                            : in std_logic;
-        valid, processingDone                                               : out std_logic;
-        loadShift, initAddress, incAddress, initSum, loadSum, loadOutput    : in std_logic;
-        v_value, h_coef                                                              : in std_logic_vector(N-1 downto 0)
+        clk                           : in std_logic;
+        initSum, loadSum, loadOutput  : in std_logic;
+        v_value, h_coef               : in std_logic_vector(N-1 downto 0);
+        sumSelect                     : in std_logic;
+
+        accOutput                     : out std_logic_vector(n_out-1 downto 0)
+
     );
-    
-
-    
-
-end FIR_OP;
 
 
 
-architecture Behavioral of FIR_OP is
+
+end accumulator;
+
+
+
+architecture Behavioral of accumulator is
 
     --type v_fir_first is ARRAY(0 to 128) of std_logic_vector(N-1 downto 0);
     --type v_fir_second is ARRAY(0 to 10) of std_logic_vector(N-1 downto 0);
     --type v_iir_no_recursive is ARRAY(0 to 2) of std_logic_vector(N-1 downto 0);
     --type v_iir_recursive is ARRAY(0 to 2) of std_logic_vector(N-1 downto 0);
-    signal buff : signed(n_out-1 downto 0);
-    
+    signal buff, w_mult   : signed(n_out-1 downto 0);
+
 begin
+
+  w_mult <= (signed(v_value) * signed(h_coef));
 
   mux_sum : process(clk) is
     begin
     if (initSum = '1') then
         buff <= (others=>'0');
-    elsif (loadSum = '1') then
-        buff <= (signed(v_value) * signed(h_coef)) + buff;
-    end if;  
-    
+    elsif (RISING_EDGE(clk) and loadSum = '1') then
+        if(sumSelect = '0') then
+          buff <= buff + w_mult;
+        else
+          buff <= buff - w_mult;
+        end if;
+    end if;
     end process mux_sum;
+
+    accOutput <= std_logic_vector(buff) when (RISING_EDGE(clk) and loadOutput = '1');
 
 end Behavioral;
